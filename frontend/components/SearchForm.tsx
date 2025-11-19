cff61229f8b772d259a5b68dce763f7e682c762c
robertguozt <robertguo69@gmail.com>
2025-11-18 23:03:55 -0800

    Update SearchForm.tsx

--- frontend/components/SearchForm.tsx
+++ frontend/components/SearchForm.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Label } from "./ui/lable";
 import {
   Select,
   SelectContent,
