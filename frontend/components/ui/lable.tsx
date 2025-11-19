dd4e9c8a072c60df096a6f00afcc5757604a54c7
robertguozt <robertguo69@gmail.com>
2025-11-18 23:04:57 -0800

    Update lable.tsx

--- frontend/components/ui/lable.tsx
+++ frontend/components/ui/lable.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import * as React from "react"
-import { cn } from "@/lib/utils"
 
 const Label = React.forwardRef<
   HTMLLabelElement,
@@ -9,10 +8,7 @@ const Label = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <label
     ref={ref}
-    className={cn(
-      "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-      className
-    )}
+    className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${className || ''}`}
     {...props}
   />
 ))
