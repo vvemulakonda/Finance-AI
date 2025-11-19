63cc3bea0d5311d6abc70139140ab0a496dd19d7
robertguozt <robertguo69@gmail.com>
2025-11-18 22:08:07 -0800

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
